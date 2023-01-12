f1fd95efd359e7741628296ac93a47884ead92b3
CWS <86645532+CCCWS@users.noreply.github.com>
2023-01-12 18:50:14 +0900

    Create HamburgerBtn.js

---
+++ HamburgerBtn/HamburgerBtn.js
@@ -0,0 +1,58 @@
+import React from "react";
+import styled, { css } from "styled-components";
+
+const HamburgerBtn = ({ btnClick, setBtnClick }) => {
+  return (
+    <Box onClick={() => setBtnClick((prev) => !prev)} btnClick={btnClick}>
+      <div />
+      <div />
+      <div />
+    </Box>
+  );
+};
+
+const Box = styled.div`
+  width: 30px;
+  height: 30px;
+  /* transform: scale(1.1); */
+
+  z-index: 1;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+
+  div {
+    transition: all ease 0.3s;
+    width: 100%;
+    height: 4px;
+    background-color: var(--gray);
+    border-radius: 30px;
+
+    transform-origin: 0px 0px;
+  }
+
+  &:hover {
+    cursor: pointer;
+  }
+
+  ${(props) =>
+    props.btnClick &&
+    css`
+      & > :first-child {
+        transform: rotate(45deg) translate(4px, -4px);
+        transform-origin: 0% 0%;
+      }
+
+      & > :nth-child(2) {
+        opacity: 0;
+      }
+
+      & > :last-child {
+        transform: rotate(-45deg) translate(4px, 4px);
+        transform-origin: 0% 100%;
+      }
+    `}
+`;
+
+export default HamburgerBtn;
