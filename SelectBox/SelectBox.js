e1c57bc342d449a2c79bf7232dfad1600fd02df0
CWS <86645532+CCCWS@users.noreply.github.com>
2023-01-20 01:11:40 +0900

    Update SelectBox.js

--- SelectBox/SelectBox.js
+++ SelectBox/SelectBox.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import styled, { css } from "styled-components";
 
 function SelectBox({
@@ -8,16 +8,13 @@ function SelectBox({
   setSelectValue,
   slide,
   fade,
+  searchBar,
 }) {
   const selectRef1 = useRef();
   const selectRef2 = useRef();
 
   const [click, setClick] = useState(false);
-
-  const select = (e) => {
-    setSelectValue(e.target.innerText);
-    setClick(!click);
-  };
+  const [searchValue, setSearchValue] = useState("");
 
   useEffect(() => {
     const clickOutside = ({ target }) => {
@@ -35,36 +32,62 @@ function SelectBox({
     };
   }, [click]);
 
-  return (
-    <Div width={width} onClick={() => setClick(!click)} ref={selectRef1}>
-      {selectValue}
+  const select = (e) => {
+    setSelectValue(e.target.innerText);
+    setClick(!click);
 
-      <Ul
-        open={click}
-        onChange={select}
-        ref={selectRef2}
-        slide={slide}
-        fade={fade}
-      >
-        {dataArr.map((item, index) => (
-          <React.Fragment key={index}>
-            {click && (
-              <Li onClick={select}>
-                {item}
-              </Li>
+    searchValue.length > 0 && setSearchValue("");
+  };
+
+  const onClickClose = (e) => {
+    e.target === e.currentTarget && setClick(!click);
+    searchValue.length > 0 && setSearchValue("");
+  };
+
+  const onSearchValue = (e) => {
+    setSearchValue(e.target.value);
+  };
+
+  return (
+    <Div width={width}>
+      <CurrSelect onClick={onClickClose} ref={selectRef1}>
+        {selectValue}
+      </CurrSelect>
+
+      <Ul open={click} ref={selectRef2} slide={slide} fade={fade}>
+        {click && (
+          <>
+            {searchBar && (
+              <Input>
+                <input
+                  placeholder="SEARCH"
+                  onChange={onSearchValue}
+                  value={searchValue}
+                />
+              </Input>
             )}
-          </React.Fragment>
-        ))}
+
+            {dataArr.map((item, index) => (
+              <React.Fragment key={index}>
+                {item.includes(searchValue) && <Li onClick={select}>{item}</Li>}
+              </React.Fragment>
+            ))}
+          </>
+        )}
       </Ul>
     </Div>
   );
 }
 
 const Div = styled.div`
+  position: relative;
   width: ${(props) => props.width};
+`;
+
+const CurrSelect = styled.div`
+  width: 100%;
 
   padding: 0.5rem;
-  margin: 0.3rem;
   font-size: 1rem;
 
   background-color: white;
@@ -97,9 +120,8 @@ const Ul = styled.ul`
   list-style: none;
   border-radius: 3px;
 
-  box-shadow: ${(props) =>
-    props.open ? "0px 0px 10px 0px rgba(0, 0, 0, 0.1)" : "none"};
-  transition: 0.3s;
+  box-shadow: ${(props) => (props.open ? "0 0 5px gray;" : "none")};
+  transition: 0.4s;
   overflow-y: scroll;
 
   ${(props) =>
@@ -114,6 +136,7 @@ const Ul = styled.ul`
       opacity: ${(props) => (props.open ? "1" : "0")};
       max-height: 15rem;
     `}
+
   &::-webkit-scrollbar {
     width: 3px;
   }
@@ -122,7 +145,7 @@ const Ul = styled.ul`
 const Li = styled.li`
   width: 100%;
 
-  padding: 1rem;
+  padding: 0.8rem;
   font-size: 1rem;
 
   color: black;
@@ -130,10 +153,37 @@ const Li = styled.li`
   display: flex;
   justify-content: center;
   align-items: center;
-  transition: all ease 0.2s;
+  transition: all ease 0.3s;
+
   &:hover {
     cursor: pointer;
     background-color: rgba(0, 0, 0, 0.1);
+  }
+`;
+
+const Input = styled.div`
+  position: sticky;
+  top: 10px;
+  width: 100%;
+  margin-bottom: 10px;
+  margin-top: 10px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  input {
+    width: 80%;
+    /* height: 10%; */
+    border-radius: 50px;
+    padding: 5px 5px 5px 10px;
+    border: none;
+    background-color: #c8c8c8;
+
+    &:focus {
+      outline: none;
+      box-shadow: 0 0 5px gray;
+    }
   }
 `;
 
