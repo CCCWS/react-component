d701ee9309e2bc4e9269fc8b69ce3381b480a09f
CWS <86645532+CCCWS@users.noreply.github.com>
2023-08-11 21:42:31 +0900

    Update FlipCard.tsx

--- FlipCard/FlipCard.tsx
+++ FlipCard/FlipCard.tsx
@@ -1,70 +1,70 @@
 import React from "react";
 import styled from "styled-components";
 
-const FlipCard = () => {
+interface CardProps {
+  width: string;
+  height: string;
+  scale?: number;
+}
+
+const FlipCard = ({ width, height, scale }: CardProps) => {
   return (
-    <Div>
-      <CardBox>
-        <Card>
-          <Front>
-            {/* <Image></Image>
+    <Card width={width} height={height} scale={scale ? scale : 1}>
+      <div>
+        <Front>
+          {/* <Image></Image>
             <Price>25,000원</Price>
             <Title>Nike Awesome Red Shoes</Title>
             <Tag>Special Edition</Tag> */}
-            <div>Front</div>
-          </Front>
+          <div>Front</div>
+        </Front>
 
-          <Back>
-            {/* <AddBtn>Add To Cart</AddBtn> */}
-            <div>Back</div>
-          </Back>
-        </Card>
-      </CardBox>
-    </Div>
+        <Back>
+          {/* <AddBtn>Add To Cart</AddBtn> */}
+          <div>Back</div>
+        </Back>
+      </div>
+    </Card>
   );
 };
 
-const Div = styled.div`
-  margin: 100px;
-`;
-
-const Card = styled.div`
-  width: inherit;
-  height: inherit;
-  transform-style: preserve-3d;
-  transition: 0.5s;
-`;
-
-const CardBox = styled.div`
-  width: 300px;
-  height: 400px;
-  perspective: 500px;
+const Card = styled.div<CardProps>`
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
   position: relative;
+  perspective: 500px;
+
+  & > :first-child {
+    width: inherit;
+    height: inherit;
+    transform-style: preserve-3d;
+    transition: 0.3s;
+  }
 
   &:hover {
-    ${Card} {
-      transform: rotateY(180deg);
+    & > :first-child {
+      transform: ${(props) => `rotateY(180deg) scale(${props.scale})`};
     }
   }
 `;
 
-const FBbase = styled.div`
+const FrontBackBase = styled.div`
   background-color: white;
   width: inherit;
   height: inherit;
 
   position: absolute;
   backface-visibility: hidden;
-  border-radius: 1em;
+  border-radius: 10px;
 
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  font-size: 3rem;
+  border: 1px solid black;
 `;
 
-const Front = styled(FBbase)``;
+const Front = styled(FrontBackBase)``;
+const Back = styled(FrontBackBase)`
+  transform: rotateY(180deg);
+`;
+
 const Image = styled.div`
   width: 14.37em;
   height: 14.37em;
@@ -74,46 +74,6 @@ const Image = styled.div`
   display: block;
 
   background-color: red;
-`;
-
-const Price = styled.h2`
-  text-align: center;
-  font-size: 2em;
-  font-weight: 300;
-  color: gray;
-`;
-const Title = styled.h3`
-  text-align: center;
-  font-size: 1em;
-  font-family: 500;
-`;
-const Tag = styled.h6`
-  text-align: center;
-  color: #707070;
-  font-weight: 500;
-  letter-spacing: 0.1em;
-`;
-
-const Back = styled(FBbase)`
-  transform: rotateY(180deg);
-`;
-const AddBtn = styled.button`
-  position: absolute;
-  transform: translate(-50%, -50%);
-  left: 50%;
-  top: 50%;
-
-  width: 12.5em;
-  font-size: 1em;
-
-  background-color: #cc091f;
-
-  color: #ffffff;
-  border-radius: 0.5em;
-  padding: 1em;
-  cursor: pointer;
-  border: none;
-  outline: none;
 `;
 
 export default FlipCard;
