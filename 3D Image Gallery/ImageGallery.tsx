1a226e149890d48f8225a8acd383f0223c5783eb
CWS <86645532+CCCWS@users.noreply.github.com>
2023-09-24 23:37:16 +0900

    ImageGallery

--- 3D Image Gallery/ImageGallery.tsx
+++ 3D Image Gallery/ImageGallery.tsx
@@ -1,142 +1,67 @@
-import React, { useState } from "react";
-import styled from "styled-components";
+import React, {
+  useCallback,
+  useEffect,
+  useState,
+  useLayoutEffect,
+  useRef,
+} from "react";
+import styled, { css } from "styled-components";
 
-interface ImageGalleryProps {
-  children: any;
-}
+import { Page, PageDiv } from "@Style/PageStyle";
 
-const ImageGallery = ({ children }: ImageGalleryProps) => {
-  console.log(children.length);
+import Ttest from "./Ttest";
+import Tttest from "./Tttest";
 
-  const calcAngle = 360 / children.length;
+import ImageGallery from "@Component/Organism/3DGallay/ImageGallery";
 
-  interface CurrLocationType {
-    index: number;
-    location: number;
-  }
+const About = () => {
+  const [click, setClick] = useState<boolean>(false);
 
-  const [currLocation, setCurrLocation] = useState<CurrLocationType>({
-    index: 0,
-    location: 0,
-  });
-
-  const onNextEvet = () => {
-    setCurrLocation({
-      index:
-        currLocation.index === children.length - 1 ? 0 : currLocation.index + 1,
-      location: currLocation.location - calcAngle,
-    });
-  };
-
-  const onPervEvet = () => {
-    setCurrLocation({
-      index:
-        currLocation.index === 0 ? children.length - 1 : currLocation.index - 1,
-      location: currLocation.location + calcAngle,
-    });
-  };
-
-  console.log(currLocation);
+  const imageArr: string[] = [
+    "gray",
+    "green",
+    "blue",
+    "yellow",
+    "pink",
+    "purple",
+  ];
 
   return (
-    <>
-      <button onClick={onPervEvet}>Perv</button>
-      <button onClick={onNextEvet}>Next</button>
+    <Page>
+      <Ttest></Ttest>
       <Test>
-        <Box currLocation={currLocation.location}>
-          <div>
-            {children.map((data: any, index: number) => (
-              <Image
-                onClick={() => console.log(data)}
-                key={index}
-                // color={data}
-                index={index}
-                currIndex={currLocation.index === index}
-                angle={calcAngle}
-              >
-                <div>{children[index]}</div>
-              </Image>
-            ))}
-          </div>
-        </Box>
+        <ImageGallery perspective={800} zAxis={300} reflect={true}>
+          {imageArr.map((data, index) => (
+            <Div key={index} color={data}>
+              {data}
+            </Div>
+          ))}
+        </ImageGallery>
       </Test>
-    </>
+    </Page>
   );
 };
 
 const Test = styled.div`
-  width: 300px;
-  height: 300px;
-  /* background-color: blue; */
+  width: 500px;
+  height: 500px;
 
-  margin: 300px;
+  background-color: blue;
 `;
 
-const Box = styled.div<{ currLocation: number }>`
+const Div = styled.div<{ color: string }>`
+  background-color: ${(props) => props.color};
+
+  width: 150px;
+  height: 150px;
+
+  border-radius: 10px;
+
   display: flex;
   justify-content: center;
   align-items: center;
 
-  background-color: gold;
-
-  width: 100%;
-  height: 100%;
-
-  transform-style: preserve-3d;
-
-  & > :first-child {
-    position: relative;
-
-    width: 100%;
-    height: 100%;
-
-    /* background-color: red; */
-
-    transform-style: preserve-3d;
-
-    transition: 1s;
-
-    transform: ${(props) =>
-      `perspective(1000px) rotateY(${props.currLocation}deg)`};
-
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
+  opacity: 1;
 `;
 
-const Image = styled.div<{
-  index: number;
-  angle: number;
-  currIndex: boolean;
-}>`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-
-  transform: translate(-50%, -50%);
-
-  transform-style: preserve-3d;
-
-  & > :first-child {
-    /* width: 100%;
-    height: 100%; */
-
-    opacity: ${(props) => (props.currIndex ? "1" : "0.1")};
-
-    border: ${(props) =>
-      props.currIndex ? "5px solid white" : "5px solid transparent"};
-
-    border-radius: 5px;
-
-    transition: 1s;
-    transform-origin: center;
-    transform: ${(props) =>
-      `rotateY(calc(${props.index} * ${props.angle}deg)) rotateX(0deg) translate3d(0,0,300px) `};
-
-    -webkit-box-reflect: below 5px
-      linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.3));
-  }
-`;
-
-export default ImageGallery;
+export default About;
