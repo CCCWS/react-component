a023726a96e08bdabd28b1a33e67b7989fe5ab46
CWS <86645532+CCCWS@users.noreply.github.com>
2023-12-29 02:37:46 +0900

    Update LoadingIcon.tsx

--- Icon/LoadingIcon.tsx
+++ Icon/LoadingIcon.tsx
@@ -10,52 +10,18 @@ const LoadingIcon = ({ size }: LoadingIconProps) => {
 };
 
 const Div = styled.div<{ size: number }>`
-  margin: 50px;
-
   width: ${(props) => `${props.size}px`};
   height: ${(props) => `${props.size}px`};
-  background-color: white;
-
+  border: 6px solid white;
+  border-right-color: gray;
   border-radius: 50%;
+  animation: spin 1s infinite linear;
 
-  position: relative;
-
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  overflow: hidden;
-
-  &::after {
-    content: "";
-    width: 70%;
-    height: 70%;
-    position: absolute;
-    background-color: white;
-    border-radius: inherit;
-  }
-
-  &::before {
-    content: "";
-    width: 100%;
-    height: 100%;
-
-    background: rgb(196, 196, 196);
-    background: linear-gradient(
-      180deg,
-      #c4c4c4 0%,
-      rgba(136, 136, 136, 1) 30%,
-      rgba(0, 0, 0, 1) 50%,
-      rgba(136, 136, 136, 1) 70%,
-      #c4c4c4 100%
-    );
-
-    animation: rotate 1.5s linear infinite;
-    /* transform-origin: 50% 50%; */
-  }
-
-  @keyframes rotate {
-    100% {
+  @keyframes spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
       transform: rotate(360deg);
     }
   }
