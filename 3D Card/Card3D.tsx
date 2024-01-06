2cf94126c6e27619cd208c5e7216b97013e24482
CWS <86645532+CCCWS@users.noreply.github.com>
2024-01-07 01:23:50 +0900

    Update Card3D.tsx

--- 3D Card/Card3D.tsx
+++ 3D Card/Card3D.tsx
@@ -1,10 +1,11 @@
 import React, { useState, useRef, useEffect, ReactNode } from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 interface CardProps {
   children: ReactNode;
   perspective?: number;
   hoverScale?: number;
+  haveParentComponent?: boolean;
 }
 
 interface CardSize {
@@ -12,12 +13,17 @@ interface CardSize {
   height: number;
 }
 
-const Card3D = ({ children, perspective, hoverScale }: CardProps) => {
+const Card3D = ({
+  children,
+  perspective,
+  hoverScale,
+  haveParentComponent,
+}: CardProps) => {
+  const cardRef = useRef<HTMLDivElement | null>(null);
   const [cardSize, setCardSize] = useState<CardSize>({
     width: 0,
     height: 0,
   });
-  const cardRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
     if (cardRef.current) {
@@ -28,70 +34,95 @@ const Card3D = ({ children, perspective, hoverScale }: CardProps) => {
     }
   }, [cardRef]);
 
-  const map = (
-    val: number,
-    minA: number,
-    maxA: number,
-    minB: number,
-    maxB: number
-  ) => {
-    return minB + ((val - minA) * (maxB - minB)) / (maxA - minA);
-  };
+  const over = (offsetX: number, offsetY: number) => {
+    const map = (
+      val: number,
+      minA: number,
+      maxA: number,
+      minB: number,
+      maxB: number
+    ) => {
+      return minB + ((val - minA) * (maxB - minB)) / (maxA - minA);
+    };
 
-  const onMouseOver = (e: React.MouseEvent) => {
-    const mouseX = e.nativeEvent.offsetX;
-    const mouseY = e.nativeEvent.offsetY;
-    const rotateY = map(mouseX, 0, cardSize.width, -25, 25); //300 > Card의 width
-    const rotateX = map(mouseY, 0, cardSize.height, 25, -25); //250 > Card의 height
-    const brightness = map(mouseY, 0, 300, 1.5, 0.7);
+    const rotateY = map(offsetX, 0, cardSize.width, -25, 25);
+    const rotateX = map(offsetY, 0, cardSize.height, 25, -25);
+    const brightness = map(offsetY, 0, 300, 1.5, 0.7);
 
     if (cardRef.current) {
       cardRef.current.style.setProperty(
         "transform",
-        `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`
+        `rotateX(${rotateX}deg) rotateY(${rotateY}deg) scale(${
+          hoverScale ? hoverScale : 1
+        })`
       );
       cardRef.current.style.setProperty("filter", `brightness(${brightness})`);
     }
   };
 
-  const onMouseLeave = () => {
+  const onMouseOver = (e: React.MouseEvent) => {
+    const offsetX = e.nativeEvent.offsetX;
+    const offsetY = e.nativeEvent.offsetY;
+
+    over(offsetX, offsetY);
+  };
+
+  const onTouchMove = (e: React.TouchEvent) => {
+    const eventTarget = e.target as HTMLDivElement;
+    const offset = eventTarget.getBoundingClientRect();
+
+    const offsetX = e.touches[0].clientX - offset.x;
+    const offsetY = e.touches[0].clientY - offset.y;
+
+    over(offsetX, offsetY);
+  };
+
+  const onMouseAndTouchLeaveEvent = () => {
     if (cardRef.current) {
       cardRef.current.style.setProperty(
         "transform",
-        "rotateX(0deg) rotateY(0deg)"
+        "rotateX(0deg) rotateY(0deg) scale(1)"
       );
       cardRef.current.style.setProperty("filter", "brightness(1)");
-      cardRef.current.style.setProperty("transform", "scale(1)");
     }
   };
 
   return (
-      <Card perspective={perspective} $hoverScale={hoverScale}>
-        <div
-          onMouseMove={(e) => onMouseOver(e)}
-          onMouseLeave={onMouseLeave}
-          ref={cardRef}
-        >
-          {children}
-        </div>
-      </Card>
+    <Card perspective={perspective} haveParentComponent={haveParentComponent}>
+      <div
+        onTouchMove={(e) => onTouchMove(e)}
+        onTouchEnd={onMouseAndTouchLeaveEvent}
+        onMouseMove={(e) => onMouseOver(e)}
+        onMouseLeave={onMouseAndTouchLeaveEvent}
+        ref={cardRef}
+      >
+        {children}
+      </div>
+    </Card>
   );
 };
 
-const Card = styled.div<{ perspective?: number; $hoverScale?: number }>`
+const Card = styled.div<{
+  perspective?: number;
+  haveParentComponent?: boolean;
+}>`
+  ${(props) =>
+    props.haveParentComponent &&
+    css`
+      width: 100%;
+      height: 100%;
+    `}
+
   display: inline-block;
   transition: 0.5s;
   perspective: ${(props) =>
     props.perspective ? `${props.perspective}px` : "1000px"};
 
-  &:hover {
-    transform: ${(props) =>
-      props.$hoverScale ? `scale(${props.$hoverScale})` : "scale(1)"};
-  }
-
   & > :first-child {
+    width: 100%;
+    height: 100%;
     transition: all 0.3s ease-out;
   }
 `;
 
-export default Card3D;
+export default React.memo(Card3D);
